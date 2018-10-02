2013ac6ae45a220ee5fecf8800e097df69c2b137
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-10-02 15:06:09 -0500

    removed duplicate document ready

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  
+  // didi's firebase
   var config = {
     apiKey: "AIzaSyDHwC2WNJYHYaVe-Qj3sOP-X3GLhgV_0Ps",
     authDomain: "color-game-chat.firebaseapp.com",
@@ -8,66 +10,60 @@ $(document).ready(function () {
     messagingSenderId: "440942527592"
   };
   firebase.initializeApp(config);
-  
-  
+
+
   // new chat box
-  
-  $(document).ready(function () {
-  
-      var name = "";
-  
-      firebase.database().ref('chat/').on('child_added',
-      function(snapshot){
-          var data = "<div id ='m'><p class ='name'>" +
-          snapshot.child('name').val() + "</p><p class ='message'>" +
-          snapshot.child('message').val() +"</p><div>";
-  
-          $("#messages").html($("#messages").html()+ data);
-      });
-  
-  
-      $("#name_submit").on("click", function () {
-          name = $("#name").val();
-          // alert(name)
-          $("#name_prompt_parent").fadeOut();
-          
-      });
-  
-      $("#send_button").on('click', function(){
-          var mess = $("#msg").val();
-          // alert(mess);
-  
-          firebase.database().ref('chat/'+ Date.now()).set({
-              name: name,
-              message: mess
-  
-  
-          });
-      
-      });
-  
+  var name = "";
+
+  firebase.database().ref('chat/').on('child_added',
+    function (snapshot) {
+      var data = "<div id ='m'><p class ='name'>" +
+        snapshot.child('name').val() + "</p><p class ='message'>" +
+        snapshot.child('message').val() + "</p><div>";
+
+      $("#messages").html($("#messages").html() + data);
+    });
+
+
+  $("#name_submit").on("click", function () {
+    name = $("#name").val();
+    // alert(name)
+    $("#name_prompt_parent").fadeOut();
+
   });
 
-// $(document).ready(function () {
+  $("#send_button").on('click', function () {
+    var mess = $("#msg").val();
+    // alert(mess);
 
-//   ticketmaster ajax call
-//   $.ajax({
-//     type: "GET",
-//     url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Nvi9ZsuaDBYE4HhFrY63AGBljsc7B9pG",
-//     async: true,
-//     dataType: "json",
-//     success: function (json) {
-//       console.log(json);
-//       // Parse the response.
-//       // Do other things.
-//     },
-//     error: function (xhr, status, err) {
-//       // This time, we do not end up here!
-//     }
-//   });
+    firebase.database().ref('chat/' + Date.now()).set({
+      name: name,
+      message: mess
 
-  //Giphy ajax call
 
+    });
+
+  });
+
+  // $(document).ready(function () {
+
+  //   ticketmaster ajax call
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Nvi9ZsuaDBYE4HhFrY63AGBljsc7B9pG",
+  //     async: true,
+  //     dataType: "json",
+  //     success: function (json) {
+  //       console.log(json);
+  //       // Parse the response.
+  //       // Do other things.
+  //     },
+  //     error: function (xhr, status, err) {
+  //       // This time, we do not end up here!
+  //     }
+  //   });
+
+  // giphy ajax call
   var queryURL = "http://api.giphy.com/v1/gifs/search?q=awesome&api_key=T3bTJBKugMxVT3yX9ddzafzVAJTHEZtk&limit=1&rating";
 
   $.ajax({ url: queryURL, method: 'GET' })
@@ -104,6 +100,3 @@ $(document).ready(function () {
   tracking.track('#myVideo', colors, { camera: true });
 
 })
-
-//Chat box 
-

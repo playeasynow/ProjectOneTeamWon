3bdebdafa6d434e6cbc2117ea62cbf5d165e131e
Efren Cavazos <efren.cavazos@gmail.com>
2018-10-01 20:35:46 -0500

    new API Key response

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,4 @@
-
-
- // Constructing a queryURL using the animal name
- var queryURL = "https://app.ticketmaster.com/discovery/v2/" +
- "&api_key=Nvi9ZsuaDBYE4HhFrY63AGBljsc7B9pG";
-
- $.ajax({
+$.ajax({
     type:"GET",
     url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Nvi9ZsuaDBYE4HhFrY63AGBljsc7B9pG",
     async:true,

2b1d2603804cf7068a8e76cdbee146c363bc924f
Efren Cavazos <efren.cavazos@gmail.com>
2018-10-07 11:17:21 -0500

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -456,4 +456,36 @@ $(".nav-item li").on("click", function () {
         fixedPosition: false                      //options: true makes it stick(fixed position) on scroll
     });
 
-});
+  });
+
+  var scrollY = 0;
+  var distance = 40;
+  var speed = 24;
+  function autoScrollTo(el) {
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+    var bodyHeight = document.body.offsetHeight;
+    var yPos = currentY + window.innerHeight;
+    var animator = setTimeout('autoScrollTo(\''+el+'\')',24);
+    if(yPos > bodyHeight){
+      clearTimeout(animator);
+    } else {
+      if(currentY < targetY-distance){
+          scrollY = currentY+distance;
+          window.scroll(0, scrollY);
+        } else {
+          clearTimeout(animator);
+        }
+    }
+  }
+  function resetScroller(el){
+    var currentY = window.pageYOffset;
+      var targetY = document.getElementById(el).offsetTop;
+    var animator = setTimeout('resetScroller(\''+el+'\')',speed);
+    if(currentY > targetY){
+      scrollY = currentY-distance;
+      window.scroll(0, scrollY);
+    } else {
+      clearTimeout(animator);
+    }
+  }
